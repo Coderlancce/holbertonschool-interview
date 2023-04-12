d08500b57330bbe97643edf4dee30be1970c78aa
Coderlancce <coderlancce@gmail.com>
2023-04-11 22:22:06 -0500

    OK minoperations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Check the number of operations "copy, paste" for get the number n
+"""
+
+def minOperations(n):
+    """
+    call getPrimo function and sum the answer for get the minOperations
+    """
+    accum = 0
+    while n > 1:
+        primo = getprimo(n)
+        n /= primo
+        accum += primo
+    
+    return accum
+
+def getPrimo(n):
+    """
+    check what primo number can use for get the result
+    """
+    primos = [2, 3, 5, 7, 11]
+    for i in primos:
+        if n % i == 0:
+            return i
