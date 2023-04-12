b3a663186f7fe7da39a2ed3399f0225b7c04de78
Coderlancce <coderlancce@gmail.com>
2023-04-12 09:02:08 -0500

    fix code

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -5,21 +5,20 @@ Check the number of operations "copy, paste" for get the number n
 
 def minOperations(n):
     """
-    call getPrimo function and sum the answer for get the minOperations
+    Calculates the fewest number of operations needed
+    to result in exactly n H characters in the file
     """
-    accum = 0
-    while n > 1:
-        primo = getPrimo(n)
-        n /= primo
-        accum += primo
-    
-    return accum
+    operations = 0
+    sum = 1
+    carrier = 0
 
-def getPrimo(n):
-    """
-    check what primo number can use for get the result
-    """
-    primos = [2, 3, 5, 7, 11]
-    for i in primos:
-        if n % i == 0:
-            return i
+    while sumation < n:
+        if n % sum == 0: #calculate if n is multiple of them
+            carrier = sum # save the last copy
+            sum *= 2 # paste
+            operations += 1 # sum the copy operation
+        else: # if no are multiple i need a mulple
+            sum += carrier # alone paste
+        operations += 1 # sum the paste operation
+    
+    return operations
