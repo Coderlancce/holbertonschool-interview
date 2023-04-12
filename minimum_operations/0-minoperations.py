772817503f09e41c58d57da2c993dfdc47c6fadf
Coderlancce <coderlancce@gmail.com>
2023-04-12 14:08:31 -0500

    OK pip 8 code style

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,6 +3,7 @@
 Check the number of operations "copy, paste" for get the number n
 """
 
+
 def minOperations(n):
     """
     Calculates the fewest number of operations needed
@@ -13,12 +14,12 @@ def minOperations(n):
     carrier = 0
 
     while sum < n:
-        if n % sum == 0: #calculate if n is multiple of them
+        if n % sum == 0:  # calculate if n is multiple of them
             carrier = sum # save the last copy
-            sum *= 2 # paste
-            operations += 1 # sum the copy operation
-        else: # if no are multiple i need a mulple
-            sum += carrier # alone paste
-        operations += 1 # sum the paste operation
-    
+            sum *= 2  # paste
+            operations += 1  # sum the copy operation
+        else:  # if no are multiple i need a mulple
+            sum += carrier  # alone paste
+        operations += 1  # sum the paste operation
+
     return operations
